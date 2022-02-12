a55da6bfdab8d6c7004e7947e6740b5d00180b51
Dan Bergren <bergren2@users.noreply.github.com>
2022-02-11 23:56:43 -0600

    create test runner (#6)

---
+++ Konigsberg.Tests/Advent/AdventProblemsEnumerator.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Konigsberg.Advent;
+using static Konigsberg.Tests.Advent.AdventTestsRunner.ProblemPart;
+
+namespace Konigsberg.Tests.Advent;
+
+public sealed partial class AdventTestsRunner
+{
+    private sealed class AdventProblemsEnumerator : IEnumerable<object[]>
+    {
+        private readonly List<object[]> _problems = new()
+        {
+            new object[] { new Year2021Day1("Year2021Day1_1.txt"), Part1, 7 },
+            new object[] { new Year2021Day1("Year2021Day1_1.txt"), Part2, 5 },
+            new object[] { new Year2021Day1("Year2021Day1_2.txt"), Part1, 1548},
+            new object[] { new Year2021Day1("Year2021Day1_2.txt"), Part2, 1589}
+        };
+
+        public IEnumerator<object[]> GetEnumerator() => _problems.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
