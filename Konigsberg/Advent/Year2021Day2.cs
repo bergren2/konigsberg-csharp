59d230e37c6ce35d77676db76f38856212d6b3f6
Dan Bergren <bergren2@users.noreply.github.com>
2022-02-12 14:42:19 -0600

    AoC 2021 Day 2 (#10)

---
+++ Konigsberg/Advent/Year2021Day2.cs
@@ -0,0 +1,91 @@
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using static Konigsberg.Helpers.ResourceHelpers;
+#pragma warning disable CS8509
+
+namespace Konigsberg.Advent;
+
+public sealed class Year2021Day2 : IAdventSolvable<int>
+{
+    private readonly ReadOnlyCollection<Command> _commands;
+
+    public Year2021Day2(string filename)
+    {
+        _commands = File.ReadAllLines(ResourcePath("Advent", filename))
+            .Select(MapToCommand)
+            .ToList()
+            .AsReadOnly();
+    }
+
+    public int SolvePart1()
+    {
+        var depth = 0;
+        var position = 0;
+        foreach (var (direction, magnitude) in _commands)
+        {
+            switch (direction)
+            {
+                case Direction.Forward:
+                    position += magnitude;
+                    break;
+                case Direction.Down:
+                    depth += magnitude;
+                    break;
+                case Direction.Up:
+                    depth -= magnitude;
+                    break;
+            }
+        }
+        return depth * position;
+    }
+
+    public int SolvePart2()
+    {
+        var depth = 0;
+        var position = 0;
+        var aim = 0;
+        foreach (var (direction, magnitude) in _commands)
+        {
+            switch (direction)
+            {
+                case Direction.Forward:
+                    position += magnitude;
+                    depth += aim * magnitude;
+                    break;
+                case Direction.Down:
+                    aim += magnitude;
+                    break;
+                case Direction.Up:
+                    aim -= magnitude;
+                    break;
+            }
+        }
+        return depth * position;
+    }
+
+    private static Command MapToCommand(string commandString)
+    {
+        var strings = commandString.Split();
+        var dir = strings[0] switch
+        {
+            "forward" => Direction.Forward,
+            "down"    => Direction.Down,
+            "up"      => Direction.Up
+        };
+        return new Command(dir, int.Parse(strings[1]));
+    }
+
+    private sealed record Command
+    (
+        Direction Direction,
+        int Magnitude
+    );
+
+    private enum Direction
+    {
+        Forward,
+        Down,
+        Up
+    }
+}
