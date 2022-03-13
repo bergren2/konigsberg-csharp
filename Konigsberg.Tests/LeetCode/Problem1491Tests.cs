ca7a5bbe69c72be503cf52a7fea0a58b73b6191a
Dan Bergren <bergren2@users.noreply.github.com>
2022-03-13 16:31:14 -0500

    add leetcode problem 1491

---
+++ Konigsberg.Tests/LeetCode/Problem1491Tests.cs
@@ -0,0 +1,18 @@
+using Konigsberg.LeetCode;
+using Xunit;
+
+namespace Konigsberg.Tests.LeetCode;
+
+public sealed class Problem1491Tests
+{
+    [Theory]
+    [InlineData(new[] { 4000, 3000, 1000, 2000 }, 2500.00000)]
+    [InlineData(new[] { 1000, 2000, 3000 }, 2000.00000)]
+    [InlineData(new[] { 1000, 2000, 30000 }, 2000.00000)]
+    public void Solve(int[] salary, int expected)
+    {
+        var problem = new Problem1491();
+        var solution = problem.Average(salary);
+        Assert.Equal(expected, solution);
+    }
+}
---
+++ Konigsberg/LeetCode/Problem1491.cs
@@ -0,0 +1,15 @@
+using System.Linq;
+
+namespace Konigsberg.LeetCode;
+
+public sealed class Problem1491
+{
+    public double Average(int[] salary)
+    {
+        var salaryList = salary.ToList();
+        salaryList.Sort();
+        salaryList.RemoveAt(salary.Length - 1);
+        salaryList.RemoveAt(0);
+        return salaryList.Average();
+    }
+}
