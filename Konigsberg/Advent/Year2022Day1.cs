7751f7f1643ff92272396b00e5e9ab677e1f535f
Dan Bergren <bergren2@users.noreply.github.com>
2022-12-03 00:07:18 -0600

    solve Day 1 Part 1

---
+++ Konigsberg/Advent/Year2022Day1.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace Konigsberg.Advent;
+
+public sealed class Year2022Day1 : IAdventSolvable<int>
+{
+    private readonly IEnumerable<IEnumerable<int>> _calories;
+
+    public Year2022Day1(string filename)
+    {
+        var lines = File.ReadAllLines(ResourcePath("Advent", filename));
+
+        var current = new List<int>();
+        var calories = new List<List<int>> { current };
+        for (var i = 0; i < lines.Length - 1; i++ )
+        {
+            if (string.IsNullOrEmpty(lines[i]))
+            {
+                current = new List<int>();
+                calories.Add(current);
+                i++;
+            }
+
+            current.Add(int.Parse(lines[i]));
+        }
+
+        _calories = calories;
+    }
+
+    public int SolvePart1() =>
+        _calories
+            .Select(x => x.Sum())
+            .Max();
+
+    public int SolvePart2()
+    {
+        throw new System.NotImplementedException();
+    }
+}
