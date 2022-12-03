fe86bf9084b4d07137ff62d044d81b73aab1e51f
Dan Bergren <bergren2@users.noreply.github.com>
2022-12-03 02:11:23 -0600

    solve Day 2

--- Konigsberg.Tests/Advent/AdventProblemsEnumerator.cs
+++ Konigsberg.Tests/Advent/AdventProblemsEnumerator.cs
@@ -15,14 +15,21 @@ public sealed partial class AdventTestsRunner
             new object[] { new Year2021Day1("Year2021Day1_2.txt"), Part1, 1548},
             new object[] { new Year2021Day1("Year2021Day1_1.txt"), Part2, 5 },
             new object[] { new Year2021Day1("Year2021Day1_2.txt"), Part2, 1589},
+
             new object[] { new Year2021Day2("Year2021Day2_1.txt"), Part1, 150 },
             new object[] { new Year2021Day2("Year2021Day2_2.txt"), Part1, 1690020 },
             new object[] { new Year2021Day2("Year2021Day2_1.txt"), Part2, 900 },
             new object[] { new Year2021Day2("Year2021Day2_2.txt"), Part2, 1408487760 },
+
             new object[] { new Year2022Day1("Year2022Day1_1.txt"), Part1, 24000 },
             new object[] { new Year2022Day1("Year2022Day1_2.txt"), Part1, 71502 },
             new object[] { new Year2022Day1("Year2022Day1_1.txt"), Part2, 45000 },
             new object[] { new Year2022Day1("Year2022Day1_2.txt"), Part2, 208191 },
+
+            new object[] { new Year2022Day2("Year2022Day2_1.txt"), Part1, 15 },
+            new object[] { new Year2022Day2("Year2022Day2_2.txt"), Part1, 13009 },
+            new object[] { new Year2022Day2("Year2022Day2_1.txt"), Part2, 12 },
+            new object[] { new Year2022Day2("Year2022Day2_2.txt"), Part2, 10398 },
         };
 
         public IEnumerator<object[]> GetEnumerator() => _problems.GetEnumerator();
